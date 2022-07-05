5c4e5362db27ed3bc0d22534844b502b437ea71a
Marek Zelinka <mzelinka17@gmail.com>
2022-07-05 13:55:21 +0200

    hide logout button if no user

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,10 +1,10 @@
 import { LogoutIcon } from "@heroicons/react/outline";
 import { Form } from "@remix-run/react";
 
-import { useUser } from "~/utils";
+import { useOptionalUser } from "~/utils";
 
 export function Navbar() {
-  useUser();
+  const user = useOptionalUser();
 
   return (
     <nav className="bg-white shadow-sm">
@@ -12,20 +12,22 @@ export function Navbar() {
         <div className="flex h-16 items-center justify-between">
           <div className="flex items-center">
             <div className="flex-shrink-0">
-              <h1 className="text-xl font-bold text-gray-800">billflow</h1>
+              <span className="text-xl font-bold text-gray-800">billflow</span>
             </div>
           </div>
-          <div className="ml-6 flex items-center">
-            <Form action="/logout" method="post">
-              <button
-                type="submit"
-                className="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-teal-500 focus-visible:ring-offset-2"
-              >
-                <span className="sr-only">Sign out</span>
-                <LogoutIcon className="h-6 w-6" />
-              </button>
-            </Form>
-          </div>
+          {user ? (
+            <div className="ml-6 flex items-center">
+              <Form action="/logout" method="post">
+                <button
+                  type="submit"
+                  className="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-teal-500 focus-visible:ring-offset-2"
+                >
+                  <span className="sr-only">Sign out</span>
+                  <LogoutIcon className="h-6 w-6" />
+                </button>
+              </Form>
+            </div>
+          ) : null}
         </div>
       </div>
     </nav>

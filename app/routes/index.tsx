38ae66ee26176631190d4d07e25954d94f66d4ec
Marek Zelinka <mzelinka17@gmail.com>
2022-07-05 13:36:36 +0200

    init

---
+++ app/routes/index.tsx
@@ -0,0 +1,39 @@
+import { Form, Link } from "@remix-run/react";
+
+import { useOptionalUser } from "~/utils";
+
+export default function Index() {
+  const user = useOptionalUser();
+
+  return (
+    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
+      <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
+        {user ? (
+          <Form action="/logout" method="post">
+            <button
+              type="submit"
+              className="flex items-center justify-center rounded-md bg-gray-500 px-4 py-3 font-medium text-white hover:bg-gray-600"
+            >
+              Logout
+            </button>
+          </Form>
+        ) : (
+          <>
+            <Link
+              to="/join"
+              className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
+            >
+              Sign up
+            </Link>
+            <Link
+              to="/login"
+              className="flex items-center justify-center rounded-md bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600"
+            >
+              Log In
+            </Link>
+          </>
+        )}
+      </div>
+    </main>
+  );
+}
---
+++ prisma/seed.js
@@ -0,0 +1,37 @@
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcryptjs");
+
+const prisma = new PrismaClient();
+
+async function seed() {
+  const email = "rachel@remix.run";
+
+  // cleanup the existing database
+  await prisma.user.delete({ where: { email } }).catch(() => {
+    // no worries if it doesn't exist yet
+  });
+
+  const hashedPassword = await bcrypt.hash("racheliscool", 10);
+
+  await prisma.user.create({
+    data: {
+      email,
+      password: {
+        create: {
+          hash: hashedPassword,
+        },
+      },
+    },
+  });
+
+  console.log(`Database has been seeded. 🌱`);
+}
+
+seed()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });

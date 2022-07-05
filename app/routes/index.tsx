5cbc0495c4d716e9b48c611a031e4ec538e2108d
Marek Zelinka <mzelinka17@gmail.com>
2022-07-05 14:08:46 +0200

    update index page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,38 +1,47 @@
-import { Form, Link } from "@remix-run/react";
+import type { LoaderFunction } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
-import { useOptionalUser } from "~/utils";
+import { getUserId } from "~/session.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const userId = await getUserId(request);
+  if (userId) {
+    return redirect("/transactions");
+  }
+
+  return json({});
+};
 
 export default function IndexPage() {
-  const user = useOptionalUser();
-
   return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
-        {user ? (
-          <Form action="/logout" method="post">
-            <button
-              type="submit"
-              className="flex items-center justify-center rounded-md bg-gray-500 px-4 py-3 font-medium text-white hover:bg-gray-600"
-            >
-              Logout
-            </button>
-          </Form>
-        ) : (
-          <>
+    <main className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl px-4">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+            <span className="block">Keep track of your</span>
+            <span className="block text-teal-600">expenses and income</span>
+          </h1>
+          <p className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
+            The best way to keep a clear view of your financial situation.
+            Perfect for keeping a record of your salary, fixed investment, stock
+            or business.
+          </p>
+          <div className="mt-5 flex flex-col gap-3 sm:mt-8 sm:flex-row sm:justify-center">
             <Link
               to="/join"
-              className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
+              className="inline-flex items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus-visible:ring-2 focus-visible:ring-teal-500 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-100"
             >
               Sign up
             </Link>
             <Link
               to="/login"
-              className="flex items-center justify-center rounded-md bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600"
+              className="inline-flex items-center justify-center rounded-md border border-transparent bg-teal-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-teal-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-teal-500 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-100"
             >
-              Log In
+              Log in
             </Link>
-          </>
-        )}
+          </div>
+        </div>
       </div>
     </main>
   );
